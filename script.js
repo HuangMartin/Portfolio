582f2572b0d2034146edb3da66e7b5d38e5304c4
HuangMartin <darkknight411032@gmail.com>
2023-04-05 18:08:11 +0800

    update

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ MenuBtn.addEventListener('click', function(e){
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark')
 })
+
+let typed = new Typed('.auto-input', {
+    strings:['軟體工程師', '部落客', '探險家'],
+    typespeed:100,
+    backspeed:100,
+    backDelay:3000,
+    loop:true,
+})
