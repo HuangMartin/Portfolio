80714af5b55fb87e3fbc298087657f9b5fc3b636
HuangMartin <darkknight411032@gmail.com>
2023-04-24 00:02:15 +0800

    update

--- finance/submit.php
+++ finance/submit.php
@@ -1,10 +1,18 @@
 <?php
+    require_once('db.php');
 
     if(isset($_POST['submitForm'])){
         $title = $_POST['title'];
         $date = $_POST['date'];
         $description = $_POST['description'];
-        echo "title: $title <br> date: $date <br> description: $description";
+
+        $sql = "INSERT INTO `data`(`title`, `date`, `description`) 
+                VALUES ('$title', '$date', '$description')";
+        if($con -> query($sql) === TRUE){
+            echo "成功加入資料庫";
+        }else{
+            echo "哪裡出錯了QQ";
+        }
     }else{
         echo "哪裡出錯了QQ";
     }
