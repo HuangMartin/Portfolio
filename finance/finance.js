ff31449616df13ed692bc841509d89ad0dfdd6b4
HuangMartin <darkknight411032@gmail.com>
2023-05-02 16:33:20 +0800

    update

--- finance/finance.js
+++ finance/finance.js
@@ -1,12 +1,5 @@
-const toggleBtn = document.querySelector('.toggle_btn')
-const toggleBtnIcon = document.querySelector('.toggle_btn i')
-const dropDownMenu = document.querySelector('.dropdown-menu')
+function getPageList(totalPages, page, maxLengh){
 
-toggleBtn.onclick = function (){
-    dropDownMenu.classList.toggle('open')
-    const isOpen = dropDownMenu.classList.contains('open')
-
-    toggleBtnIcon.classList = isOpen
-    ? 'fa-solid fa-xmark'
-    : 'fa-solid fa-bars'
 }
+
+// 18:56
