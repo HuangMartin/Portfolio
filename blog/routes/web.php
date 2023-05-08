612abcd9a8a3e0a54bb9f0f071e51fa8a597b0fb
HuangMartin <darkknight411032@gmail.com>
2023-05-09 00:07:23 +0800

    update

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,11 +14,5 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', function () {
     return view('home');
 });
-
-// Route::get('/',[HomeController::class,'home']);
