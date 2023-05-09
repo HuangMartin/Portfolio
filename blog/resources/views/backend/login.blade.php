80aae5c9d8b86c7f857843154e11a6b401b41639
HuangMartin <darkknight411032@gmail.com>
2023-05-09 16:18:35 +0800

    update

--- blog/resources/views/backend/login.blade.php
+++ blog/resources/views/backend/login.blade.php
@@ -1,50 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Admin Login</title>
-    <link href="{{asset('backend')}}/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="{{asset('backend')}}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
-    <link href="{{asset('backend')}}/css/sb-admin.css" rel="stylesheet" />
-</head>
+  <head>
 
-<body class="bg-primary">
-    <div id="layoutAuthentication">
-        <div id="layoutAuthentication_content">
-            <main>
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-lg-5">
-                            <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                <div class="card-header">
-                                    <h3 class="text-center font-weight-light my-4">Login</h3>
-                                </div>
-                                <div class="card-body">
-                                    <form method="post">
-                                        <div class="form-floating mb-3">
-                                            <input class="form-control" id="inputEmail" type="email" name="username" />
-                                            <label for="inputEmail">電子郵件/使用者名稱</label>
-                                        </div>
-                                        <div class="form-floating mb-3">
-                                            <input class="form-control" id="inputPassword" type="password" name="password" />
-                                            <label for="inputPassword">密碼</label>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </main>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin Login</title>
+
+    <!-- Bootstrap core CSS-->
+    <link href="{{asset('backend')}}/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template-->
+    <link href="{{asset('backend')}}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template-->
+    <link href="{{asset('backend')}}/css/sb-admin.css" rel="stylesheet">
+
+  </head>
+
+  <body class="bg-dark">
+
+    <div class="container">
+      <div class="card card-login mx-auto mt-5">
+        <div class="card-header">登入</div>
+        <div class="card-body">
+          @if($errors)
+            @foreach($errors->all() as $error)
+            <p class="text-danger">{{$error}}</p>
+            @endforeach
+          @endif
+
+          @if(Session::has('error'))
+          <p class="text-danger">{{session('error')}}</p>
+          @endif
+
+          <form method="post">
+            @csrf
+            <div class="form-group">
+              <div class="form-label-group">
+                <input type="text" id="inputEmail" class="form-control" name="username" placeholder="Username" />
+                <label for="inputEmail">使用者名稱</label>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="form-label-group">
+                <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Password" />
+                <label for="inputPassword">密碼</label>
+              </div>
+            </div>
+            <input type="submit" class="btn btn-primary btn-block" />
+          </form>
         </div>
+      </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="js/scripts.js"></script>
-</body>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{asset('backend')}}/vendor/jquery/jquery.min.js"></script>
+    <script src="{{asset('backend')}}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('backend')}}/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  </body>
 
 </html>
