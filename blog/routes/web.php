8e429d289c420309f21407ecf23e91aeb2ccbb84
HuangMartin <darkknight411032@gmail.com>
2023-05-13 01:44:08 +0800

    update

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use \App\Http\Controllers\AuthorController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,14 +18,3 @@ Route::get('/', function () {
 });
 
 
-Route::prefix('author')->name('author.')->group(function(){
-
-    Route::middleware(['guest:web'])->group(function(){
-        Route::view('/login','back.pages.auth.login')->name('login');
-        Route::view('/forgot-password','back.pages.auth.forgot')->name('forgot-password');
-    });
-
-    Route::middleware([])->group(function(){
-        Route::get('/home',[AuthorController::class, 'index'])->name('home');
-    });
-});
