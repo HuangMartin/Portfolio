2ab719c8c6ded8357c0bf1388cc969ee824a4061
HuangMartin <darkknight411032@gmail.com>
2023-05-13 14:17:18 +0800

    update

--- blog/app/Http/Livewire/AuthorLoginForm.php
+++ blog/app/Http/Livewire/AuthorLoginForm.php
@@ -3,9 +3,43 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthorLoginForm extends Component
 {
+    public $email, $password;
+
+    public function LoginHandler()
+    {
+        $this->validate([
+            'email'=>'required|email|exists:users,email',
+            'password'=>'required|min:5'
+        ],[
+            'email.required'=>'請輸入email!',
+            'email.email'=>'Invaild email address',
+            'email.exists'=>'這個email不存在於資料庫中!',
+            'password.required'=>'密碼是必填項目!',
+        ]);
+
+        $creds = array('email'=>$this->email, 'password'=>$this->password);
+
+        if( Auth::guard('web')->attempt($creds) ){
+
+            $checkUser = User::where('email', $this->email)->first();
+            if($checkUser->blocked == 1){
+                Auth::guard('web')->logout();
+                return redirect()->route('author.login')->with('fail','Your account had been blocked.');
+            }else{
+                return redirect()->route('author.home');
+            }
+
+
+
+        }else{
+            session()->flash('fail','email或密碼錯誤');
+        }
+    }
     public function render()
     {
         return view('livewire.author-login-form');
--- blog/resources/views/livewire/author-login-form.blade.php
+++ blog/resources/views/livewire/author-login-form.blade.php
@@ -1,6 +1,11 @@
 <div>
 
     @if(Session::get('fail'))
+      <div class="alert alter-danger">
+        {{Session::get('fail')}}
+      </div>
+    @endif
+
 
     <form wire:submit.prevent="LoginHandler()" method="post" autocomplete="off" novalidate="">
         <div class="mb-3">
@@ -24,10 +29,10 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 12m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path><path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7"></path></svg>
               </a>
             </span>
-            @error('password')
-              <span class="text-danger">{{ $message }}</span>
-            @enderror
           </div>
+          @error('password')
+              <span class="text-danger">{{ $message }}</span>
+          @enderror
         </div>
         <div class="mb-2">
           <label class="form-check">
