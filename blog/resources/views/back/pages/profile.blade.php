16994524acbf028ae6fce7346ff5ae802e3a2cba
HuangMartin <darkknight411032@gmail.com>
2023-05-18 11:56:24 +0800

    update

--- blog/resources/views/back/pages/profile.blade.php
+++ blog/resources/views/back/pages/profile.blade.php
@@ -16,7 +16,6 @@
                         <div class="col-auto">
                             <a href="#" class="text-reset">UI Designer at Tabler</a>
                         </div>
-                        
                     </div>
                 </div>
             </div>
@@ -31,32 +30,59 @@
     <div class="row">
         <div class="card">
             <div class="card-header">
-              <ul class="nav nav-tabs card-header-tabs" data-bs-toggle="tabs">
-                <li class="nav-item">
-                  <a href="#tabs-home-ex1" class="nav-link active" data-bs-toggle="tab">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a href="#tabs-profile-ex1" class="nav-link" data-bs-toggle="tab">Profile</a>
-                </li>
-              </ul>
+                <ul class="nav nav-tabs card-header-tabs" data-bs-toggle="tabs">
+                    <li class="nav-item">
+                        <a href="#tabs-details" class="nav-link active" data-bs-toggle="tab">個人信息</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#tabs-password" class="nav-link" data-bs-toggle="tab">變更密碼</a>
+                    </li>
+                </ul>
             </div>
             <div class="card-body">
-              <div class="tab-content">
-                <div class="tab-pane active show" id="tabs-home-ex1">
-                  <h4>Home tab</h4>
-                  <div>Cursus turpis vestibulum, dui in pharetra vulputate id sed non turpis ultricies fringilla at sed facilisis lacus pellentesque purus nibh</div>
+                <div class="tab-content">
+                    <div class="tab-pane active show" id="tabs-details">
+                        <div>
+                          <form method="post">
+                            <div class="row">
+                              <div class="col-md-4">
+                                <div class="mb-3">
+                                  <label class="form-label">名字</label>
+                                  <input type="text" class="form-control" name="example-text-input" placeholder="name">
+                                </div>
+                              </div>
+                              <div class="col-md-4">
+                                <div class="mb-3">
+                                  <label class="form-label">使用者名稱</label>
+                                  <input type="text" class="form-control" name="example-text-input" placeholder="username">
+                                </div>
+                              </div>
+                              <div class="col-md-4">
+                                <div class="mb-3">
+                                  <label class="form-label">Email</label>
+                                  <input type="text" class="form-control" name="example-text-input" placeholder="email" disabled>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="mb-3">
+                              <label class="form-label">簡介</label>
+                              <textarea class="form-control" name="example-textarea-input" rows="6" placeholder="寫點什麼吧...">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, quia!</textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">保存變更</button>
+                          </form>
+                        </div>
+                    </div>
+                    <div class="tab-pane" id="tabs-password">
+                        <div>Fringilla egestas nunc quis tellus diam rhoncus ultricies tristique enim at diam, sem nunc
+                            amet, pellentesque id egestas velit sed</div>
+                    </div>
                 </div>
-                <div class="tab-pane" id="tabs-profile-ex1">
-                  <h4>Profile tab</h4>
-                  <div>Fringilla egestas nunc quis tellus diam rhoncus ultricies tristique enim at diam, sem nunc amet, pellentesque id egestas velit sed</div>
-                </div>
-              </div>
             </div>
-          </div>
-          
+        </div>
+
     </div>
 
 
 @endsection
 
-{{-- 6:14 --}}
+{{-- 10.33 --}}
