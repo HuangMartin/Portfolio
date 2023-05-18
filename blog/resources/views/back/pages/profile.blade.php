d9275a74fffdb76ba69832842537e7686068e259
HuangMartin <darkknight411032@gmail.com>
2023-05-18 13:22:31 +0800

    update

--- blog/resources/views/back/pages/profile.blade.php
+++ blog/resources/views/back/pages/profile.blade.php
@@ -2,30 +2,7 @@
 @section('pageTitle', isset($pageTitle) ? $pageTitle : 'Profile')
 @section('content')
 
-
-
-    <div class="page-header">
-        <div class="row align-items-center">
-            <div class="col-auto">
-                <span class="avatar avatar-md" style="background-image: url(/samples/avatars/023m.jpg)"></span>
-            </div>
-            <div class="col-md-6">
-                <h2 class="page-title">Paweł Kuna</h2>
-                <div class="page-subtitle">
-                    <div class="row">
-                        <div class="col-auto">
-                            <a href="#" class="text-reset">UI Designer at Tabler</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-auto d-md-flex">
-                <a href="#" class="btn btn-primary">
-                    更換圖片
-                </a>
-            </div>
-        </div>
-    </div>
+@livewire('author-profile-header')
     <hr>
     <div class="row">
         <div class="card">
@@ -84,5 +61,3 @@
 
 
 @endsection
-
-{{-- 10.33 --}}
