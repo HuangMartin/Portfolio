388e29c0a0fa136e25d9ceae8afd5bd9728d6d3c
HuangMartin <darkknight411032@gmail.com>
2023-05-20 01:19:36 +0800

    update

--- blog/resources/views/livewire/author-change-password-form.blade.php
+++ blog/resources/views/livewire/author-change-password-form.blade.php
@@ -1,24 +1,30 @@
 <div>
     
 
-    <form method="post">
+    <form method="post" wire:submit.prevent='changePassword()'>
         <div class="row">
             <div class="col-md-4">
                 <div class="mb-3">
                     <label class="form-label">目前密碼</label>
-                    <input type="password" class="form-control" name="example-text-input" placeholder="Current password">
+                    <input type="password" class="form-control" name="example-text-input" placeholder="目前密碼"
+                    wire:model='current_password'>
+                    <span class="text-danger">@error('current_password'){{ $message }}@enderror</span>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="mb-3">
                     <label class="form-label">新密碼</label>
-                    <input type="password" class="form-control" name="example-text-input" placeholder="New password">
+                    <input type="password" class="form-control" name="example-text-input" placeholder="新密碼"
+                    wire:model='new_password'>
+                    <span class="text-danger">@error('new_password'){{ $message }}@enderror</span>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="mb-3">
                     <label class="form-label">確認新密碼</label>
-                    <input type="password" class="form-control" name="example-text-input" placeholder="Type new password again">
+                    <input type="password" class="form-control" name="example-text-input" placeholder="確認新密碼"
+                    wire:model='confirm_new_password'>
+                    <span class="text-danger">@error('confirm_new_password'){{ $message }}@enderror</span>
                 </div>
             </div>
         </div>
--- blog/resources/views/livewire/author-personal-details.blade.php
+++ blog/resources/views/livewire/author-personal-details.blade.php
@@ -5,21 +5,21 @@
             <div class="col-md-4">
                 <div class="mb-3">
                     <label class="form-label">姓名</label>
-                    <input type="text" class="form-control" name="example-text-input" placeholder="name" wire:model='name'>
+                    <input type="text" class="form-control" name="example-text-input" placeholder="姓名" wire:model='name'>
                     <span class="text-danger">@error('name'){{ $message }}@enderror</span>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="mb-3">
                     <label class="form-label">使用者名稱</label>
-                    <input type="text" class="form-control" name="example-text-input" placeholder="Username" wire:model='username'>
+                    <input type="text" class="form-control" name="example-text-input" placeholder="使用者名稱" wire:model='username'>
                     <span class="text-danger">@error('username'){{ $message }}@enderror</span>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="mb-3">
                     <label class="form-label">Email</label>
-                    <input type="text" class="form-control" name="example-text-input" placeholder="email" disabled wire:model='email'>
+                    <input type="text" class="form-control" name="example-text-input" placeholder="Email" disabled wire:model='email'>
                     <span class="text-danger">@error('email'){{ $message }}@enderror</span>
                 </div>
             </div>
