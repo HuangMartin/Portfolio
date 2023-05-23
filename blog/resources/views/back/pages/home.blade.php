1a145f1e6c456ca09dfc656be01492109b64bc05
Martin <darkknight411032@gmail.com>
2023-05-23 16:25:36 +0800

    update

--- blog/resources/views/back/pages/home.blade.php
+++ blog/resources/views/back/pages/home.blade.php
@@ -1,5 +1,8 @@
 @extends('back.layouts.pages-layout')
 @section('pageTitle', isset($pageTitle) ? $pageTitle : 'Home')
 @section('content')
-    123
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nisi dolore eveniet quibusdam laborum praesentium
+    quod magnam voluptates mollitia qui iure recusandae facere ad rerum, veniam nulla itaque ducimus ratione odio expedita
+    ullam et, pariatur nemo architecto! Modi, culpa similique et odio adipisci iure consequatur, delectus laborum
+    voluptates, dolorum sint.
 @endsection
