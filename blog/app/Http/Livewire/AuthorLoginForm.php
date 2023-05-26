219ef234493433a39abf1933358fc00e10372978
Martin <darkknight411032@gmail.com>
2023-05-27 01:29:36 +0800

    update

--- blog/app/Http/Livewire/AuthorLoginForm.php
+++ blog/app/Http/Livewire/AuthorLoginForm.php
@@ -3,9 +3,15 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthorLoginForm extends Component
 {
+    public $email, $password;
+    public function LoginHandler(){
+        dd('123');
+    }
     public function render()
     {
         return view('livewire.author-login-form');
